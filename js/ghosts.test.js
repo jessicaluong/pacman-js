dcaa68c65a4e15c68d6035a03f6968fb521ad521
jessicaluong <96930184+jessicaluong@users.noreply.github.com>
2024-05-16 10:23:45 -0700

    Add unit tests for Ghost methods in ghosts.js

--- js/ghosts.test.js
+++ js/ghosts.test.js
@@ -1,182 +1,307 @@
-import { Ghost } from "./ghosts";
-import { MazeManager } from "./mazeManager";
+/**
+ * @jest-environment jsdom
+ */
+import { Ghost, GhostManager } from "./ghosts";
 import { cellSize } from "./setup";
 
-function createMockImage(src) {
-  return {
-    src: src,
-    onload: jest.fn(),
-    onerror: jest.fn(),
-  };
-}
-
-describe("reset", () => {
-  it("should reset properties to their initial values", () => {
-    const images = [createMockImage("path/to/ghost.png")];
-    const ghost = new Ghost("blinky", null, null, cellSize, images, {
-      position: { x: 3, y: 3 },
-    });
-    ghost.reset();
-    expect(ghost.position).toEqual(ghost.initialPosition);
-    expect(ghost.mode).toBe("random");
-    expect(ghost.lastDirection.dx).toBe(0);
-    expect(ghost.lastDirection.dy).toBe(0);
-  });
-});
-
-describe("Ghost movement", () => {
-  test("should only move to valid positions", () => {
-    const mockMaze = [
-      [1, 1, 1, 1, 1],
-      [1, 0, 0, 0, 1],
-      [1, 0, 1, 0, 1],
-      [1, 0, 0, 0, 1],
-      [1, 1, 1, 1, 1],
-    ];
-
-    const mazeManager = new MazeManager(mockMaze);
-    const images = [createMockImage("path/to/ghost.png")];
-    const ghost = new Ghost("blinky", null, mazeManager, cellSize, images, {
-      position: { x: 3, y: 3 },
-    });
-
-    const tries = 20;
-    for (let i = 0; i < tries; i++) {
-      ghost.chooseRandomDirection();
-      let ghostGridPosX = Math.floor(ghost.position.x / cellSize);
-      let ghostGridPosY = Math.floor(ghost.position.y / cellSize);
-
-      expect(mazeManager.hasWall(ghostGridPosX, ghostGridPosY)).toBe(false);
-    }
-  });
-
-  test("should not move to invalid position", () => {
-    const mockMaze = [
-      [1, 1, 1],
-      [1, 0, 1],
-      [1, 1, 1],
-    ];
-    const mazeManager = new MazeManager(mockMaze);
-    const images = [createMockImage("path/to/ghost.png")];
-    const ghost = new Ghost("blinky", null, mazeManager, cellSize, images, {
-      position: { x: 1, y: 1 },
-    });
-
-    const tries = 20;
-    for (let i = 0; i < tries; i++) {
-      ghost.chooseRandomDirection();
-      let ghostGridPosX = Math.floor(ghost.position.x / cellSize);
-      let ghostGridPosY = Math.floor(ghost.position.y / cellSize);
-
-      expect(ghostGridPosX).toBe(1);
-      expect(ghostGridPosY).toBe(1);
-    }
-  });
-
-  test("moves left correctly from position 0,1 to 1,1 wrapping around", () => {
-    const mockMaze = [
+global.window = {};
+window.PF = {
+  Grid: jest.fn().mockImplementation((matrix) => ({
+    clone: jest.fn().mockReturnThis(),
+  })),
+  AStarFinder: jest.fn().mockImplementation(() => ({
+    findPath: jest.fn().mockReturnValue([
       [1, 1],
-      [0, 0],
-      [1, 1],
-    ];
-    const mazeManager = new MazeManager(mockMaze);
-    const images = [createMockImage("path/to/ghost.png")];
-    const ghost = new Ghost("blinky", null, mazeManager, cellSize, images, {
-      position: { x: 0, y: 1 },
-    });
+      [1, 2],
+    ]),
+  })),
+};
 
-    ghost.lastDirection = { dx: -1 * ghost.velocity, dy: 0 };
-    ghost.chooseRandomDirection();
+describe("Ghost", () => {
+  let ghost, mockCtx, mockMazeManager;
 
-    let ghostGridPosX = Math.floor(ghost.position.x / cellSize);
-    let ghostGridPosY = Math.floor(ghost.position.y / cellSize);
-
-    expect(ghostGridPosX).toBe(1);
-    expect(ghostGridPosY).toBe(1);
-  });
-
-  test("moves right correctly from position {maze width},1 to 0,1 wrapping around", () => {
-    const mockMaze = [
-      [1, 1],
-      [0, 0],
-      [1, 1],
-    ];
-    const mazeManager = new MazeManager(mockMaze);
-
-    // Place ghost at the very edge of the maze using maze width
-    const images = [createMockImage("path/to/ghost.png")];
-    const ghost = new Ghost("blinky", null, mazeManager, cellSize, images, {
-      position: { x: mazeManager.getMazeWidth(), y: 1 },
-    });
-
-    ghost.lastDirection = { dx: 1 * ghost.velocity, dy: 0 };
-    ghost.chooseRandomDirection();
-
-    let ghostGridPosX = Math.floor(ghost.position.x / cellSize);
-    let ghostGridPosY = Math.floor(ghost.position.y / cellSize);
-
-    expect(ghostGridPosX).toBe(0);
-    expect(ghostGridPosY).toBe(1);
-  });
-
-  describe("should not reverse the current direction if path is clear", () => {
-    const mockMaze = [
-      [1, 1, 1, 1],
-      [1, 0, 0, 1],
-      [1, 0, 0, 1],
-      [1, 1, 1, 1],
-    ];
-    const images = [createMockImage("path/to/ghost.png")];
-    const mazeManager = new MazeManager(mockMaze);
-    let ghost;
-
-    beforeEach(() => {
-      ghost = new Ghost("blinky", null, mazeManager, cellSize, images, {
-        position: { x: 1, y: 1 },
-      });
-    });
-
-    test("should not reverse to left when moving right if path is clear", () => {
-      ghost.lastDirection = { dx: 1 * ghost.velocity, dy: 0 }; // Initially moving right
-      ghost.chooseRandomDirection();
-      expect(ghost.lastDirection.dx).not.toBe(-1 * ghost.velocity); // Should not choose left
-    });
-
-    test("should not reverse to up when moving down if path is clear", () => {
-      ghost.lastDirection = { dx: 0, dy: 1 * ghost.velocity }; // Initially moving down
-      ghost.chooseRandomDirection();
-      expect(ghost.lastDirection.dy).not.toBe(-1 * ghost.velocity); // Should not choose up
-    });
-  });
-});
-
-describe("Ghost drawn", () => {
-  test("flips image when moving left", () => {
-    const mockCtx = {
+  beforeEach(() => {
+    mockCtx = {
       save: jest.fn(),
       restore: jest.fn(),
       translate: jest.fn(),
-      scale: jest.fn(),
+      rotate: jest.fn(),
       drawImage: jest.fn(),
+      scale: jest.fn(),
     };
-    const images = [createMockImage("path/to/ghost.png")];
-    const ghost = new Ghost("blinky", mockCtx, null, cellSize, images, {
-      position: { x: 1, y: 1 },
+
+    mockMazeManager = {
+      getMazeWidth: jest.fn().mockReturnValue(3),
+      hasWall: jest.fn(),
+      hasInvisibleWall: jest.fn(),
+      getMaze: jest.fn().mockReturnValue([
+        [0, 1, 0],
+        [0, 0, 0],
+        [1, 0, 1],
+      ]),
+    };
+
+    ghost = new Ghost(
+      "blinky",
+      mockCtx,
+      mockMazeManager,
+      cellSize,
+      [new Image(), new Image(), new Image()],
+      { position: { x: 1, y: 1 } }
+    );
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  describe("frighten mode", () => {
+    beforeEach(() => {
+      originalSetTimeout = global.setTimeout;
+      originalClearTimeout = global.clearTimeout;
+      originalSetInterval = global.setInterval;
+      originalClearInterval = global.clearInterval;
+
+      global.setTimeout = jest
+        .fn()
+        .mockImplementation((fn, delay) => "timeoutId");
+      global.clearTimeout = jest.fn();
+      global.setInterval = jest
+        .fn()
+        .mockImplementation((fn, interval) => "intervalId");
+      global.clearInterval = jest.fn();
     });
 
-    ghost.lastDirection = { dx: -1 * ghost.velocity, dy: 0 }; // Left
-    ghost.draw();
+    afterEach(() => {
+      global.setTimeout = originalSetTimeout;
+      global.clearTimeout = originalClearTimeout;
+      global.setInterval = originalSetInterval;
+      global.clearInterval = originalClearInterval;
+    });
 
-    expect(mockCtx.save).toHaveBeenCalled();
-    expect(mockCtx.scale).toHaveBeenCalledWith(-1, 1);
-    expect(mockCtx.translate).toHaveBeenCalledWith(-60, 0);
-    expect(mockCtx.drawImage).toHaveBeenCalledWith(
-      ghost.images[0],
-      20,
-      20,
-      cellSize,
-      cellSize
-    );
-    expect(mockCtx.restore).toHaveBeenCalled();
+    test("frighten should set mode to frighten and handle timers correctly", () => {
+      ghost.frightenedTimer = global.setTimeout(() => {}, 1000);
+
+      ghost.frighten();
+      expect(ghost.mode).toBe("frighten");
+      expect(ghost.currentImage).toBe(ghost.images[1]);
+      expect(global.clearTimeout).toHaveBeenCalled();
+      expect(global.setTimeout).toHaveBeenCalledWith(
+        expect.any(Function),
+        ghost.frightenedDuration - ghost.blinkDuration
+      );
+      expect(ghost.frightenedTimer).toBe("timeoutId");
+    });
+
+    test("startBlinking should toggle images and set correct timers", () => {
+      ghost.startBlinking();
+      expect(global.setInterval).toHaveBeenCalledWith(
+        expect.any(Function),
+        ghost.blinkInterval
+      );
+      expect(ghost.blinkTimer).toBe("intervalId");
+      expect(global.setTimeout).toHaveBeenCalledWith(
+        expect.any(Function),
+        ghost.blinkDuration
+      );
+    });
+
+    test("endFrighten should reset mode to random and clear timers", () => {
+      ghost.frighten();
+      ghost.blinkTimer = "intervalId";
+
+      ghost.endFrighten();
+      expect(ghost.mode).toBe("random");
+      expect(ghost.currentImage).toBe(ghost.images[0]);
+      expect(global.clearInterval).toHaveBeenCalledWith("intervalId");
+      expect(ghost.blinkTimer).toBeNull();
+      expect(ghost.frightenedTimer).toBeNull();
+      expect(ghost.lastDirection).toEqual({ dx: 0, dy: 0 });
+    });
+  });
+
+  describe("reset", () => {
+    beforeEach(() => {
+      originalClearTimeout = global.clearTimeout;
+      originalClearInterval = global.clearInterval;
+
+      global.clearTimeout = jest.fn();
+      global.clearInterval = jest.fn();
+    });
+
+    afterEach(() => {
+      global.clearTimeout = originalClearTimeout;
+      global.clearInterval = originalClearInterval;
+    });
+
+    test("should restore initial state and clear timers", () => {
+      ghost.frightenedTimer = "timeoutId";
+      ghost.blinkTimer = "intervalId";
+
+      ghost.reset();
+      expect(ghost.position).toEqual(ghost.initialPosition);
+      expect(ghost.mode).toBe("random");
+      expect(ghost.currentImage).toBe(ghost.images[0]);
+      expect(ghost.lastDirection).toEqual({ dx: 0, dy: 0 });
+      expect(global.clearInterval).toHaveBeenCalledWith("intervalId");
+      expect(global.clearTimeout).toHaveBeenCalledWith("timeoutId");
+    });
+  });
+
+  describe("draw", () => {
+    test("should handle image flip when moving left", () => {
+      ghost.lastDirection = { dx: -2, dy: 0 }; // Moving left
+      ghost.draw();
+      expect(mockCtx.save).toHaveBeenCalled();
+      expect(mockCtx.scale).toHaveBeenCalledWith(-1, 1);
+      expect(mockCtx.translate).toHaveBeenCalledWith(-60, 0); // Translated position
+      expect(mockCtx.drawImage).toHaveBeenCalledWith(
+        ghost.currentImage,
+        ghost.position.x,
+        ghost.position.y,
+        ghost.cellSize,
+        ghost.cellSize
+      );
+      expect(mockCtx.restore).toHaveBeenCalled();
+    });
+
+    test("should not flip image when moving right", () => {
+      ghost.lastDirection = { dx: 2, dy: 0 }; // Moving right
+      ghost.draw();
+      expect(mockCtx.save).toHaveBeenCalled();
+      expect(mockCtx.scale).not.toHaveBeenCalled();
+      expect(mockCtx.translate).not.toHaveBeenCalled();
+      expect(mockCtx.drawImage).toHaveBeenCalledWith(
+        ghost.currentImage,
+        ghost.position.x,
+        ghost.position.y,
+        ghost.cellSize,
+        ghost.cellSize
+      );
+      expect(mockCtx.restore).toHaveBeenCalled();
+    });
+  });
+
+  describe("isCollideWithWall", () => {
+    test("should return true if any corner collides with a wall", () => {
+      mockMazeManager.hasWall.mockReturnValue(false);
+      mockMazeManager.hasInvisibleWall.mockReturnValue(true); // One corner hits an invisible wall
+      const result = ghost.isCollideWithWall(100, 100);
+      expect(result).toBeTruthy();
+      expect(mockMazeManager.hasWall).toHaveBeenCalledTimes(1);
+      expect(mockMazeManager.hasInvisibleWall).toHaveBeenCalledTimes(1);
+    });
+
+    test("should return false if no corners collide with a wall", () => {
+      mockMazeManager.hasWall.mockReturnValue(false);
+      mockMazeManager.hasInvisibleWall.mockReturnValue(false);
+      const result = ghost.isCollideWithWall(100, 100);
+      expect(result).toBeFalsy();
+      expect(mockMazeManager.hasWall).toHaveBeenCalledTimes(4);
+      expect(mockMazeManager.hasInvisibleWall).toHaveBeenCalledTimes(4);
+    });
+  });
+
+  describe("move", () => {
+    test("should choose random direction in random mode", () => {
+      ghost.mode = "random";
+      const spy = jest.spyOn(ghost, "chooseRandomDirection");
+      ghost.move({ x: 0, y: 0 });
+      expect(spy).toHaveBeenCalled();
+    });
+
+    test("should choose A* in chase mode", () => {
+      ghost.mode = "chase";
+      const spy = jest.spyOn(ghost, "chooseDirectionUsingAStar");
+      ghost.move({ x: 1, y: 2 });
+      expect(spy).toHaveBeenCalledWith(1, 2);
+    });
+  });
+
+  describe("chooseRandomDirection", () => {
+    beforeEach(() => {
+      jest.spyOn(ghost, "isCollideWithWall");
+    });
+
+    test("should update position and last direction correctly", () => {
+      ghost.isCollideWithWall.mockReturnValue(false);
+      ghost.lastDirection = { dx: 2, dy: 0 }; // Moving right
+
+      ghost.chooseRandomDirection();
+      expect(ghost.isCollideWithWall).toHaveBeenCalledTimes(4);
+      expect(ghost.position).not.toBe({ x: 20, y: 20 }); // Position should change
+      expect(ghost.lastDirection).not.toEqual({ dx: -2, dy: 0 }); // Should not reverse direction
+    });
+
+    test("filters out reverse directions and collisions", () => {
+      ghost.isCollideWithWall
+        .mockReturnValueOnce(false) // Left
+        .mockReturnValueOnce(true) // Right, collides
+        .mockReturnValueOnce(true) // Up, collides
+        .mockReturnValueOnce(false); // Down
+      ghost.lastDirection = { dx: 2, dy: 0 }; // Moving right
+
+      ghost.chooseRandomDirection();
+      expect(ghost.isCollideWithWall).toHaveBeenCalledTimes(4);
+
+      // Should move down
+      expect(ghost.position).not.toBe({ x: 20, y: 22 });
+      expect(ghost.lastDirection).toEqual({ dx: 0, dy: 2 });
+    });
+  });
+
+  describe("chase mode", () => {
+    test("setupPathfindingGrid initializes grid and finder correctly", () => {
+      expect(window.PF.Grid).toHaveBeenCalled();
+      expect(window.PF.AStarFinder).toHaveBeenCalled();
+      expect(ghost.grid).toBeDefined();
+      expect(ghost.finder).toBeDefined();
+    });
+
+    test("toGridPosition adjusts x position based on positive x direction", () => {
+      const pos = ghost.toGridPosition(105, ghost.cellSize, { dx: 2, dy: 0 });
+      expect(pos).toBe(5);
+    });
+
+    test("toGridPosition adjusts x position based on negative x direction", () => {
+      const pos = ghost.toGridPosition(105, ghost.cellSize, { dx: -2, dy: 0 });
+      expect(pos).toBe(6);
+    });
+
+    test("toGridPosition adjusts y position based on positive y direction", () => {
+      const pos = ghost.toGridPosition(105, ghost.cellSize, { dx: 0, dy: 2 });
+      expect(pos).toBe(5);
+    });
+
+    test("toGridPosition adjusts y position based on negative y direction", () => {
+      const pos = ghost.toGridPosition(105, ghost.cellSize, { dx: 0, dy: -2 });
+      expect(pos).toBe(6);
+    });
+
+    test("chooseDirectionUsingAStar computes correct path and updates position", () => {
+      jest.spyOn(ghost, "updatePositionBasedOnPath");
+      ghost.chooseDirectionUsingAStar(140, 140);
+      expect(ghost.grid.clone).toHaveBeenCalled();
+      expect(ghost.finder.findPath).toHaveBeenCalledWith(
+        1,
+        1,
+        7,
+        7,
+        ghost.grid
+      );
+      expect(ghost.updatePositionBasedOnPath).toHaveBeenCalledWith([1, 2]);
+    });
+
+    test("updatePositionBasedOnPath moves ghost to the next step correctly (down)", () => {
+      ghost.updatePositionBasedOnPath([1, 5]);
+      expect(ghost.lastDirection.dx).toBe(0);
+      expect(ghost.lastDirection.dy).toBe(1 * ghost.velocity);
+    });
+
+    test("updatePositionBasedOnPath moves ghost to the next step correctly (left)", () => {
+      ghost.updatePositionBasedOnPath([0, 1]);
+      expect(ghost.lastDirection.dx).toBe(-1 * ghost.velocity);
+      expect(ghost.lastDirection.dy).toBe(0);
+    });
   });
 });
